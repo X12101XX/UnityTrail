c9e1c8b5592e1d0af180fffe80058d319f332ab4
X12101X <X12101XX@outlook.com>
2026-03-29 21:43:07 +0800

    修改了Skill的结构
    将Skill从直接实现整个技能，改为了提供接口，具体的实现由角色来实现
    对于SkillBase，提供了active 和 can_activate
    两个函数，可以交给具体技能来实现激活技能以及判定当时是否可以激活技能
    
    同时，对于DashSkill，也提供了一个示范实现
    其中，值得注意的是，对于决定一个技能是否可以释放的外部条件，不应当写进
    can_activate，应该将其系统性因素，如CD放进can_activate中

--- Scripts/Skills/DashSkill.gd
+++ Scripts/Skills/DashSkill.gd
@@ -1,56 +1,18 @@
-extends "res://Scripts/Skills/SkillBase.gd"
+extends SkillBase
+class_name DashSkill
 
-@export var dash_speed := 1500.0
-@export var dash_time := 0.18
-@export var cooldown := 0.2
 
-var _cooling := false
-# false -> 未冷却
-# true -> 冷却中
-
-func activate(user: CharacterBody2D):
-	if _cooling or user.last_direction == Vector2.ZERO:
+func activate(user) -> void:
+	if not can_activate(user):
 		return
 
-	_cooling = true
-	user.is_dashing = true
+	if not user.has_method("can_dash") or not user.has_method("start_dash"):
+		return
 
-	# 设置冲刺方向和冲刺计时器
-	var dash_dir: Vector2 = user.last_direction
-	dash_dir.normalized()
-	
-	var dash_timer = dash_time
+	if not user.can_dash():
+		return
 
-	# 获取角色贴图
-	var sprite:Sprite2D = user.get_node("Sprite2D")
+	if user.last_direction == Vector2.ZERO:
+		return
 
-	while dash_timer > 0:
-		# 在贴图位置生成 trail
-		var trail := Sprite2D.new()
-		
-		# 设置特效采用的贴图，生成尾迹
-		trail.texture = sprite.texture
-		trail.global_position = sprite.global_position
-		trail.offset = Vector2.ZERO
-		user.get_parent().add_child(trail)
-		
-		# 补间动画
-		var tw = trail.create_tween()
-		tw.tween_property(trail, "modulate:a", 0.0, 0.15)
-		tw.tween_callback(trail.queue_free)
-		
-		# 设置冲刺速度
-		user.velocity.x = dash_dir.x * dash_speed
-		user.velocity.y = dash_dir.y * dash_speed
-
-		dash_timer -= get_physics_process_delta_time()
-		await get_tree().process_frame
-
-	# 冲刺后硬直
-	user.velocity = Vector2(0, 0)
-
-	user.is_dashing = false
-
-	# 冷却
-	await get_tree().create_timer(cooldown).timeout
-	_cooling = false
+	user.start_dash(user.last_direction)
--- Scripts/Skills/SkillBase.gd
+++ Scripts/Skills/SkillBase.gd
@@ -1,4 +1,15 @@
 extends Node
+class_name SkillBase
 
-func activate(user):
-	push_warning("SkillBase.activate() not implemented")
+@export var skill_name := ""
+@export var enabled := true
+
+
+func activate(user) -> void:
+	# 实际技能的实现
+	pass
+
+
+func can_activate(user) -> bool:
+	# 默认允许，子类可重写
+	return enabled
